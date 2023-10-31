0af5279205be61870db60af009801ee17b2235d9
plla2 <jhbljs92@naver.com>
2023-10-31 15:20:32 +0900

    Routes 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/organs/Navbar";
 import "./index.css";
+import Home from "./components/pages/Home";
 
 const App = () => {
   return (
-    <>
+    <div className="w-full h-full bg-zinc-900 font-mono relative text-white">
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
 
