c5e249ff79acadc7e1df580a0d8284a72f13fa29
plla2 <jhbljs92@naver.com>
2023-11-02 22:38:37 +0900

    react-slick css 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/organs/Navbar";
 import "./index.css";
 import Home from "./components/pages/Home";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
-    <div className="w-full h-full bg-zinc-900 font-mono relative text-white">
+    <div className="w-full h-full bg-zinc-900 font-mono relative text-zinc-300">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
