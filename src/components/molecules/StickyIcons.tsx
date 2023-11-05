cccdee38ac194bbe0ed869166cc5cb679fe9cc71
plla2 <jhbljs92@naver.com>
2023-11-05 16:07:41 +0900

    Style: 고정소셜바 style

--- src/components/molecules/StickyIcons.tsx
+++ src/components/molecules/StickyIcons.tsx
@@ -4,14 +4,26 @@ import { AiFillApple } from "@react-icons/all-files/ai/AiFillApple";
 
 const StickyIcons = () => {
   return (
-    <aside>
-      <a href="https://jhbljs92.tistory.com/" target="_blank">
+    <aside className="fixed lg:bottom-0  bottom-1/2 left-0 flex flex-col gap-5 items-center bg-gradient-to-t z-40 from-red-500 to-amber-500 rounded-r-lg py-3 px-2 ">
+      <a
+        href="https://jhbljs92.tistory.com/"
+        target="_blank"
+        className="text-zinc-100 hover:text-zinc-400"
+      >
         <AiFillEdit />
       </a>
-      <a href="https://github.com/plla2" target="_blank">
+      <a
+        href="https://github.com/plla2"
+        target="_blank"
+        className="text-zinc-100 hover:text-zinc-400"
+      >
         <AiFillGithub />
       </a>
-      <a href="https://plla2.netlify.app/" target="_blank">
+      <a
+        href="https://plla2.netlify.app/"
+        target="_blank"
+        className="text-zinc-100 hover:text-zinc-400"
+      >
         <AiFillApple />
       </a>
     </aside>
