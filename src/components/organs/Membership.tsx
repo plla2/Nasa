6b06745350614e22acf29a971bcc585992c92216
plla2 <jhbljs92@naver.com>
2023-11-11 15:10:28 +0900

    feat: Memberships organs 추가

--- src/components/organs/Membership.tsx
+++ src/components/organs/Membership.tsx
@@ -1,5 +1,6 @@
-import Card from "../atoms/Card";
+import List from "../atoms/List";
 import Text from "../atoms/Text";
+import Card from "../molecules/Card";
 import { MembershipPlans } from "../particles/Data";
 
 const Membership = () => {
@@ -18,7 +19,23 @@ const Membership = () => {
       </div>
       <main>
         {MembershipPlans.cards.map((card, index) => (
-          <Card />
+          <Card className="" key={index}>
+            <Text as="h2" className="">
+              <span>$</span>
+              <span>{card.amount}</span>
+              <span>/{card.duration}</span>
+            </Text>
+            <Text as="h3" className="">
+              {card.caption}
+            </Text>
+            <ul>
+              {card.benefits.map((benefit, index) => (
+                <List className="" key={index}>
+                  {benefit}
+                </List>
+              ))}
+            </ul>
+          </Card>
         ))}
       </main>
     </section>
