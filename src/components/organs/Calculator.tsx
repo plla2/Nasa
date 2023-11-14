b5406edee9f6aa1182b113c4c383894569d53d3b
plla2 <jhbljs92@naver.com>
2023-11-14 21:44:51 +0900

    feat: Calculator organs 추가

--- src/components/organs/Calculator.tsx
+++ src/components/organs/Calculator.tsx
@@ -1,10 +1,39 @@
+import { Fade } from "react-awesome-reveal";
+import Text from "../atoms/Text";
 import Form from "../molecules/Form";
+import Image from "../atoms/Image";
+import CalImg from "../../assets/gym/9.jpeg";
 
 const Calculator = () => {
   return (
-    <div>
-      <Form />
-    </div>
+    <section>
+      <main>
+        <div>
+          <Fade>
+            <div>
+              <Text as="p" className="">
+                Advanced Calculator
+              </Text>
+              <Text as="h1" className="">
+                Calculate Your BMI
+              </Text>
+              <Text as="h1" className="">
+                04
+              </Text>
+            </div>
+            <Form />
+          </Fade>
+        </div>
+        <div>
+          <Image
+            alt="CalImg"
+            objectCover="object-cover"
+            className=""
+            image={CalImg}
+          />
+        </div>
+      </main>
+    </section>
   );
 };
 
