8ddc78ad1092c5d368b1906371b7ad9fe1bc9b0f
plla2 <jhbljs92@naver.com>
2023-11-20 21:04:08 +0900

    feat: scrolltoTop 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/organs/Navbar";
 import "./index.css";
 import Home from "./components/pages/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useEffect } from "react";
+import { animateScroll } from "react-scroll";
 
 const App = () => {
+  const directory = useLocation();
+  useEffect(() => {
+    animateScroll.scrollToTop({
+      duration: 0,
+    });
+  }, [directory.pathname]);
   return (
     <div className="w-full h-full bg-zinc-900 font-mono relative text-zinc-300">
       <Navbar />
