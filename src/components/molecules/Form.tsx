3893852fcd81557674372dba73ee06c13e166035
plla2 <jhbljs92@naver.com>
2023-11-21 15:43:48 +0900

    feat: Form mole style

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -36,10 +36,15 @@ const Form = () => {
   };
 
   return (
-    <main>
-      <InputField className="">
-        <label htmlFor="height">Height</label>
-        <div>
+    <main className="w-full grid gap-6 pb-10 md:pb-0">
+      <InputField className="w-full flex flex-col gap-2">
+        <label
+          htmlFor="height"
+          className="uppercase text-sm text-zinc-200 font-bold"
+        >
+          Height
+        </label>
+        <div className="relative">
           <input
             type="number"
             name="height"
@@ -49,41 +54,58 @@ const Form = () => {
               setHeightValue(e.target.value)
             }
             placeholder="키를 입력해주세요."
+            className="w-full h-12 pl-4 pr-8 text-zinc-400 transition-all duration-200 focus:border-amber-500 focus:outline-none border border-zinc-400 bg-transparent"
           />
-          <Text className="" as="span">
+          <Text
+            className="absolute text-zinc-200 font-bold top-3 right-3"
+            as="span"
+          >
             cm
           </Text>
         </div>
       </InputField>
-      <InputField className="">
-        <label htmlFor="Weight">Weight</label>
-        <div>
+      <InputField className="w-full flex flex-col gap-2">
+        <label
+          htmlFor="Weight"
+          className="text-sm font-bold uppercase text-zinc-200"
+        >
+          Weight
+        </label>
+        <div className="relative">
           <input
             type="number"
             name="weight"
             id="weight"
             value={weightValue}
             placeholder="몸무게를 입력해주세요."
+            required
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
               setWeightValue(e.target.value)
             }
+            className="w-full h-12 border border-zinc-400 pl-4 pr-8 transition-all duration-200 focus:border-amber-500 focus:outline-none text-zinc-400 bg-transparent"
           />
-          <Text className="" as="span">
+          <Text
+            className="absolute top-3 right-3 text-zinc-200 font-bold"
+            as="span"
+          >
             kg
           </Text>
         </div>
       </InputField>
-      <div>
-        <Button onClick={calculateBmi} className="">
+      <div className="w-full mt-4 flex justify-center">
+        <Button
+          onClick={calculateBmi}
+          className="px-6 py-3 bg-gradient-to-r to-amber-500 from-red-500 text-zinc-200 text-sm uppercase font-semibold rounded-md"
+        >
           BMI 계산하기
         </Button>
       </div>
       {bmiValue && (
-        <div>
-          <Text className="" as="h2">
+        <div className="w-full flex flex-col p-4 bg-zinc-700">
+          <Text className="text-zinc-200 text-lg" as="h2">
             당신의 BMI는 {bmiValue}입니다.
           </Text>
-          <Text className="" as="p">
+          <Text className="text-amber-500 mt-3" as="p">
             {bmiMessage}
           </Text>
         </div>
